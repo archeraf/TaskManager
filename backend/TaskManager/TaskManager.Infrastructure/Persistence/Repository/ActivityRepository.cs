08e8014c7a0607e55a650d5962a31a20750c63fb
Archer <antlipe@hotmail.com>
2025-11-29 00:42:51 -0300

    Adding repositories

---
+++ backend/TaskManager/TaskManager.Infrastructure/Persistence/Repository/ActivityRepository.cs
@@ -0,0 +1,14 @@
+﻿using TaskManager.Domain.Entities;
+using TaskManager.Infrastructure.Persistence.Context;
+using TaskManager.Infrastructure.Persistence.Repository.Generic;
+
+namespace TaskManager.Infrastructure.Persistence.Repository
+{
+    internal class ActivityRepository : GenericRepository<Activity>
+    {
+        public ActivityRepository(MySqlContext context) : base(context)
+        {
+        }
+
+    }
+}
---
+++ backend/TaskManager/TaskManager.Infrastructure/Persistence/Repository/ProjectRepository.cs
@@ -0,0 +1,13 @@
+﻿using TaskManager.Domain.Entities;
+using TaskManager.Infrastructure.Persistence.Context;
+using TaskManager.Infrastructure.Persistence.Repository.Generic;
+
+namespace TaskManager.Infrastructure.Persistence.Repository
+{
+    public class ProjectRepository : GenericRepository<Project>
+    {
+        public ProjectRepository(MySqlContext context) : base(context)
+        {
+        }
+    }
+}
